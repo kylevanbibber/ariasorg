a2151887758ba0495b706db50ef6334b9237f297
kylevanbibber <108955710+kylevanbibber@users.noreply.github.com>
2023-10-04 19:49:41 -0400

    new

--- agency/server.js
+++ agency/server.js
@@ -27,85 +27,87 @@ app.use(auth(config));
 
 function ensureLoggedIn(req, res, next) {
     if (req.oidc.isAuthenticated()) {
-      return next();
+        return next();
     }
     res.redirect('/login');
-  }
+}
 
 // Test endpoint
 app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 // Create a new agent
 app.post('/agents', (req, res) => {
-  const { agent_name, contract_level } = req.body;
-  const sql = 'INSERT INTO agent_table (agent_name, contract_level) VALUES (?, ?)';
-  const agentData = [agent_name, contract_level];
-  
-
-  db.query(sql, agentData, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    res.status(201).json({ message: 'New agent created.', AgentID: result.insertId });
-  });
+    const { agent_name, contract_level } = req.body;
+    const sql = 'INSERT INTO agent_table (agent_name, contract_level) VALUES ($1, $2)';
+    db.query(sql, [agent_name, contract_level], (err, result) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: 'Internal Server Error', details: err.message });
+            return;
+        }
+        res.status(201).json({ message: 'New agent created.', AgentID: result.insertId });
+    });
 });
 
 // Read all agents
 app.get('/agents', (req, res) => {
-  const sql = 'SELECT * FROM agent_table';
-
-  db.query(sql, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    res.status(200).json(results);
-  });
+    const sql = 'SELECT * FROM agent_table';
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: 'Internal Server Error', details: err.message });
+            return;
+        }
+        res.status(200).json(results);
+    });
 });
 
 // Update an agent
-app.put('/agents/:id', (req, res) => {
-  const { id } = req.params;
-  const { FirstName, LastName, Email, PhoneNumber, Position } = req.body;
-  const sql = 'UPDATE agent_table SET ? WHERE AgentID = ?';
-  const agentData = { FirstName, LastName, Email, PhoneNumber, Position };
-
-  db.query(sql, [agentData, id], (err, result) => {
-    if (err) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    res.status(200).json({ message: 'Agent updated.' });
+app.put('/agents/:agent_code', (req, res) => {
+  const { agent_code } = req.params;
+  const { agent_name, contract_level } = req.body;
+  const sql = 'UPDATE agent_table SET agent_name = $1, contract_level = $2 WHERE agent_code = $3';
+  db.query(sql, [agent_name, contract_level, agent_code], (err, result) => {
+      if (err) {
+          console.error(err.message);
+          res.status(500).json({ error: 'Internal Server Error', details: err.message });
+          return;
+      }
+      res.status(200).json({ message: 'Agent updated.' });
   });
 });
 
 // Delete an agent
-app.delete('/agents/:id', (req, res) => {
-  const { id } = req.params;
-  const sql = 'DELETE FROM agent_table WHERE AgentID = ?';
-
-  db.query(sql, id, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    res.status(200).json({ message: 'Agent deleted.' });
+app.delete('/agents/:agent_code', (req, res) => {
+  const { agent_code } = req.params;
+  const sql = 'DELETE FROM agent_table WHERE agent_code = $1';
+  db.query(sql, [agent_code], (err, result) => {
+      if (err) {
+          console.error(err.message);
+          res.status(500).json({ error: 'Internal Server Error', details: err.message });
+          return;
+      }
+      res.status(200).json({ message: 'Agent deleted.' });
   });
 });
 
+
 // Profile page, only accessible for logged-in users
 app.get('/profile', ensureLoggedIn, (req, res) => {
-  res.json({
-    user: req.oidc.user,
-  });
+    res.json({
+        user: req.oidc.user,
+    });
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
-res.status(500).send({ error: 'Internal Server Error', details: err.message });
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Internal Server Error', details: err.message });
+});
